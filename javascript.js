29ecfeed69bcfd37f89da19ae2dd1663d6cf28a7
kiseart <iseart.journey@gmail.com>
2022-11-21 12:06:14 +0100

    thingy

---
+++ javascript.js
@@ -0,0 +1,32 @@
+const container = document.querySelector("#container");
+container.style.display =  "flex";
+container.style.justifyContent = "center";
+
+const canvas = document.createElement("div");
+canvas.style.display = "flex";
+canvas.style.flexDirection = "column";
+canvas.style.width = "auto";
+canvas.style.backgroundColor = "#e0e1e3";
+canvas.style.border = "2px solid black";
+canvas.style.justifyContent = "space-around";
+
+for (let i=0; i<64; i++){
+    let horizontal = document.createElement("div");
+    for (let j=0; j<64; j++){
+        let cell = document.createElement("div");
+        cell.style.backgroundColor = "#99b1e1";
+        cell.style.width = "12px";
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = "#e16931";
+        });
+        horizontal.appendChild(cell);
+    };
+    horizontal.style.display = "flex";
+    horizontal.style.height = "12px";
+    horizontal.style.justifyContent = "space-around";
+    canvas.appendChild(horizontal);
+};
+
+
+
+container.appendChild(canvas);
